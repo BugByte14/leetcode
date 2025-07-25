3f312df2afb4d0898e6faaee3bb83377d31b0c16
William <wmr_1014@yahoo.com>
2025-07-25 11:17:53 -0500

    Python 3487

---
+++ Python/3487.py
@@ -0,0 +1,79 @@
+"""
+3487. Maximum Unique Subarray Sum After Deletion
+Easy
+Topics
+premium lock iconCompanies
+Hint
+
+You are given an integer array nums.
+
+You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, select a
+
+of nums such that:
+
+    All elements in the subarray are unique.
+    The sum of the elements in the subarray is maximized.
+
+Return the maximum sum of such a subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+
+Output: 15
+
+Explanation:
+
+Select the entire array without deleting any element to obtain the maximum sum.
+
+Example 2:
+
+Input: nums = [1,1,0,1,1]
+
+Output: 1
+
+Explanation:
+
+Delete the element nums[0] == 1, nums[1] == 1, nums[2] == 0, and nums[3] == 1. Select the entire array [1] to obtain the maximum sum.
+
+Example 3:
+
+Input: nums = [1,2,-1,-2,1,0,-1]
+
+Output: 3
+
+Explanation:
+
+Delete the elements nums[2] == -1 and nums[3] == -2, and select the subarray [2, 1] from [1, 2, 1, 0, -1] to obtain the maximum sum.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 100
+    -100 <= nums[i] <= 100
+"""
+
+"""
+Time Complexity: O(n)
+Runtime: 0ms
+Memory: 12.33 MB
+"""
+
+class Solution(object):
+    def maxSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        curr_num, max_sum = 0, 0
+        if nums[-1] <= 0:
+            return nums[-1]
+        for i in nums:
+            if i > curr_num:
+                max_sum += i
+                curr_num = i
+        return max_sum
